4077eb002b4dad4636c86f881f5362b74ad7a100
msmontenegro3 <mirta.s.montenegro.l@gmail.com>
2025-11-13 13:19:09 -0500

    fix: update CORS configuration for Render HTTPS

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,31 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
+  const allowedOrigins = [
+    'http://localhost:4200',
+    'https://tampu-frontend.onrender.com',
+  ];
+
   app.enableCors({
+    origin: (origin, callback) => {
+      // Permite peticiones sin origin (como Postman o SSR)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.warn('❌ Bloqueado por CORS:', origin);
+        callback(new Error('No permitido por CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  // Habilitar CORS
+  /* app.enableCors({
     origin: [
       'http://localhost:4200', // local
       'https://tampu-frontend.onrender.com', // producción
@@ -14,6 +37,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000); */
 }
 bootstrap();
